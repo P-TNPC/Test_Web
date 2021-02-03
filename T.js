98fb3a6c8fac3905c5f8685c1e16c3466e0067e3
TNPC <62324946+P-TNPC@users.noreply.github.com>
2021-02-04 02:42:04 +0800

    Update T.js

--- T.js
+++ T.js
@@ -1,13 +1,5 @@
 $.ajax({
-    url: 'http://127.0.0.1:8011/D.php',
-	dataType: 'xml',
-	success: function (data){
-        var boom = $(data).find("[dn='day']").find("[cityname='化州市']");
-		var 城 = boom.attr('cityname');
-        var 现 = boom.attr('temNow');
-		var 类 = boom.attr('stateDetailed');
-        var 时 = boom.attr('time');
-        $('#气').html(TESTING);
+    $('#气').html(TESTING);
     }
 }
 );
