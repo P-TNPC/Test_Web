957bd5e591d0a16a7d1d14b011371dbfbc08163a
TNPC <62324946+P-TNPC@users.noreply.github.com>
2022-06-24 09:11:52 +0800

    Update 生成.js

--- PsRdInt/生成.js
+++ PsRdInt/生成.js
@@ -1,55 +1,58 @@
-function 出(){
-    var 量 = document.getElementById('量').value;
-    var 小 = document.getElementById('小').value;if(!小){小=0;}
-    var 大 = document.getElementById('大').value;if(!大){大=0;}
-    if(小>大){
-        var 介 = 小;
-        	小 = 大;
-        	大 = 介;
-    }
-    if(量=='0'){
-        var 出 = '呐，你知道吗？樱花下落的速度是秒速五厘米，是克里门特·沃洛什洛夫-2重型坦克的152.4毫米M-10T榴弹炮发射PB-35APCBC被帽风帽穿甲弹时炮口初速的八千七百二十分之一，是发射 OF-530HE榴弹时炮口初速的一万零六千分之一噢~<br>紧靠在一起的两颗心，会在500米的距离内，被穿甲弹炸出来的破片砸个稀巴烂，而榴弹里的6.86千克炸药会把他们轰得渣都不剩。<br>世界上没有什么力量比爱情更强大，除了KV-2重型坦克与它的152毫米主炮。';
-        document.getElementById('出').className='';
-    }
-    else{
-        var 出 = 生(量,小,大);
-        if(量<=2){
-            document.getElementById('出').className='双';
-        }
-        else if(量<=4){
-            document.getElementById('出').className='叒';
-        }
-        else if(量<=8){
-            document.getElementById('出').className='叕';
-        }
-        else{
-            document.getElementById('出').className='';
-        }
-    }
-    document.getElementById('出').innerHTML=出;
+document.querySelector('#生').onmousedown = function () {
+	if ("vibrate" in navigator) {
+		// vibration API supported
+		navigator.vibrate([15, 100, 10]);
+	} else if (navigator.webkitVibrate) {
+		navigator.webkitVibrate([15, 100, 10]);
+	}
 }
-function 生(量,小,大){
-    if(!量){
-        量 = 1;
-    }
-    if(小||大){
-        var 组 = new Array();
-        for(var i=0;
-        i<量;
-        i++){
-            组.push(整(小,大));
-        }
-    }
-    else{
-        var 组 = new Uint32Array(量);
-        window.crypto.getRandomValues(组);
-    }
-    /*
-    for(i in 组){
-    console.log(组[i])}
-    */
-    return 组.toString().replace(/,/g,", ");
-}
-function 整(小,大){
-    return Math.floor(Math.random()*(大 - 小 + 1)) + parseInt(小);
-}
+
+document.addEventListener('DOMContentLoaded', () => {
+	var duration = 750;
+	// 样式string拼凑
+	var forStyle = (position) => {
+		var cssStr = '';
+		for (let key in position) {
+			cssStr += key + ':' + position[key] + ';';
+		};
+		return cssStr;
+	}
+	// 获取点击位置
+	var forRect = (target) => {
+		var position = target.getBoundingClientRect(),
+			ele = document.documentElement;
+		return {
+			top: position.top + window.pageYOffset - ele.clientTop,
+			left: position.left + window.pageXOffset - ele.clientLeft
+		}
+	}
+	var show = (event) => {
+		var pDiv = event.target,
+			cDiv = document.createElement('div');
+		pDiv.appendChild(cDiv);
+		var rectObj = forRect(pDiv),
+			_height = event.pageY - rectObj.top,
+			_left = event.pageX - rectObj.left,
+			_scale = 'scale(' + pDiv.clientWidth / 100 * 10 + ')';
+		var position = {
+			top: _height + 'px',
+			left: _left + 'px'
+		};
+		cDiv.className = cDiv.className + " 波";
+		cDiv.setAttribute("style", forStyle(position));
+		var finishStyle = {
+			opacity: 0,
+			transform: _scale,
+			top: _height + "px",
+			left: _left + "px",
+		};
+		setTimeout(() => {
+			cDiv.setAttribute("style", forStyle(finishStyle));
+			setTimeout(() => {
+				pDiv.removeChild(cDiv);
+			}, duration);
+		}, 100);
+	}
+	document.querySelector('#罩').addEventListener('click', e => show(e), !1);
+	document.querySelector('#生').addEventListener('click', e => show(e), !1);
+}, !1);
