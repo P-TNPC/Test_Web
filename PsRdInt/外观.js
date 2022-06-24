0909e503290d59ae2a574fd41a502da76fe6d9c5
TNPC <62324946+P-TNPC@users.noreply.github.com>
2022-06-24 09:15:52 +0800

    Update 外观.js

--- PsRdInt/外观.js
+++ PsRdInt/外观.js
@@ -1,172 +1,58 @@
-document.getElementById("生").onmousedown = function (){
-    if ("vibrate" in navigator) {
+document.querySelector('#生').onmousedown = function () {
+	if ("vibrate" in navigator) {
 		// vibration API supported
-		navigator.vibrate([15,100,10]);
-    }else if (navigator.webkitVibrate) {
-        navigator.webkitVibrate([15,100,10]);
-    }
+		navigator.vibrate([15, 100, 10]);
+	} else if (navigator.webkitVibrate) {
+		navigator.webkitVibrate([15, 100, 10]);
+	}
 }
-document.addEventListener('DOMContentLoaded',
-function() {
+
+document.addEventListener('DOMContentLoaded', () => {
 	var duration = 750;
 	// 样式string拼凑
-	var forStyle = function(position) {
+	var forStyle = (position) => {
 		var cssStr = '';
-		for (var key in position) {
-			if (position.hasOwnProperty(key)) cssStr += key + ':' + position[key] + ';';
+		for (let key in position) {
+			cssStr += key + ':' + position[key] + ';';
 		};
 		return cssStr;
 	}
-	// 获取鼠标点击位置
-	var forRect = function(target) {
-		var position = {
-			top: 0,
-			left: 0
-		},
-		ele = document.documentElement;
-		'undefined' != typeof target.getBoundingClientRect && (position = target.getBoundingClientRect());
+	// 获取点击位置
+	var forRect = (target) => {
+		var position = target.getBoundingClientRect(),
+			ele = document.documentElement;
 		return {
 			top: position.top + window.pageYOffset - ele.clientTop,
 			left: position.left + window.pageXOffset - ele.clientLeft
 		}
 	}
-	var show = function(event) {
+	var show = (event) => {
 		var pDiv = event.target,
-		cDiv = document.createElement('div');
+			cDiv = document.createElement('div');
 		pDiv.appendChild(cDiv);
 		var rectObj = forRect(pDiv),
-		_height = event.pageY - rectObj.top,
-		_left = event.pageX - rectObj.left,
-		_scale = 'scale(' + pDiv.clientWidth / 100 * 10 + ')';
+			_height = event.pageY - rectObj.top,
+			_left = event.pageX - rectObj.left,
+			_scale = 'scale(' + pDiv.clientWidth / 100 * 10 + ')';
 		var position = {
 			top: _height + 'px',
 			left: _left + 'px'
 		};
-		cDiv.className = cDiv.className + " waves-animation",
-		cDiv.setAttribute("style", forStyle(position)),
-		position["-webkit-transform"] = _scale,
-		position["-moz-transform"] = _scale,
-		position["-ms-transform"] = _scale,
-		position["-o-transform"] = _scale,
-		position.transform = _scale,
-		position.opacity = "1",
-		position["-webkit-transition-duration"] = duration + "ms",
-		position["-moz-transition-duration"] = duration + "ms",
-		position["-o-transition-duration"] = duration + "ms",
-		position["transition-duration"] = duration + "ms",
-		position["-webkit-transition-timing-function"] = "cubic-bezier(0.250, 0.460, 0.450, 0.940)",
-		position["-moz-transition-timing-function"] = "cubic-bezier(0.250, 0.460, 0.450, 0.940)",
-		position["-o-transition-timing-function"] = "cubic-bezier(0.250, 0.460, 0.450, 0.940)",
-		position["transition-timing-function"] = "cubic-bezier(0.250, 0.460, 0.450, 0.940)",
+		cDiv.className = cDiv.className + " 波";
 		cDiv.setAttribute("style", forStyle(position));
 		var finishStyle = {
 			opacity: 0,
-			"-webkit-transition-duration": duration + "ms",
-			"-moz-transition-duration": duration + "ms",
-			"-o-transition-duration": duration + "ms",
-			"transition-duration": duration + "ms",
-			"-webkit-transform": _scale,
-			"-moz-transform": _scale,
-			"-ms-transform": _scale,
-			"-o-transform": _scale,
+			transform: _scale,
 			top: _height + "px",
 			left: _left + "px",
 		};
-		setTimeout(function() {
+		setTimeout(() => {
 			cDiv.setAttribute("style", forStyle(finishStyle));
-			setTimeout(function() {
+			setTimeout(() => {
 				pDiv.removeChild(cDiv);
-			},
-			duration);
-		},
-		100)
+			}, duration);
+		}, 100);
 	}
-	document.querySelector('.waves').addEventListener('click',
-	function(e) {
-		show(e);
-	},
-	!1);
-},
-!1);
-document.addEventListener('DOMContentLoaded',
-function() {
-	var duration = 750;
-	// 样式string拼凑
-	var forStyle = function(position) {
-		var cssStr = '';
-		for (var key in position) {
-			if (position.hasOwnProperty(key)) cssStr += key + ':' + position[key] + ';';
-		};
-		return cssStr;
-	}
-	// 获取鼠标点击位置
-	var forRect = function(target) {
-		var position = {
-			top: 0,
-			left: 0
-		},
-		ele = document.documentElement;
-		'undefined' != typeof target.getBoundingClientRect && (position = target.getBoundingClientRect());
-		return {
-			top: position.top + window.pageYOffset - ele.clientTop,
-			left: position.left + window.pageXOffset - ele.clientLeft
-		}
-	}
-	var show = function(event) {
-		var pDiv = event.target,
-		cDiv = document.createElement('div');
-		pDiv.appendChild(cDiv);
-		var rectObj = forRect(pDiv),
-		_height = event.pageY - rectObj.top,
-		_left = event.pageX - rectObj.left,
-		_scale = 'scale(' + pDiv.clientWidth / 100 * 10 + ')';
-		var position = {
-			top: _height + 'px',
-			left: _left + 'px'
-		};
-		cDiv.className = cDiv.className + " wave-animation",
-		cDiv.setAttribute("style", forStyle(position)),
-		position["-webkit-transform"] = _scale,
-		position["-moz-transform"] = _scale,
-		position["-ms-transform"] = _scale,
-		position["-o-transform"] = _scale,
-		position.transform = _scale,
-		position.opacity = "1",
-		position["-webkit-transition-duration"] = duration + "ms",
-		position["-moz-transition-duration"] = duration + "ms",
-		position["-o-transition-duration"] = duration + "ms",
-		position["transition-duration"] = duration + "ms",
-		position["-webkit-transition-timing-function"] = "cubic-bezier(0.250, 0.460, 0.450, 0.940)",
-		position["-moz-transition-timing-function"] = "cubic-bezier(0.250, 0.460, 0.450, 0.940)",
-		position["-o-transition-timing-function"] = "cubic-bezier(0.250, 0.460, 0.450, 0.940)",
-		position["transition-timing-function"] = "cubic-bezier(0.250, 0.460, 0.450, 0.940)",
-		cDiv.setAttribute("style", forStyle(position));
-		var finishStyle = {
-			opacity: 0,
-			"-webkit-transition-duration": duration + "ms",
-			"-moz-transition-duration": duration + "ms",
-			"-o-transition-duration": duration + "ms",
-			"transition-duration": duration + "ms",
-			"-webkit-transform": _scale,
-			"-moz-transform": _scale,
-			"-ms-transform": _scale,
-			"-o-transform": _scale,
-			top: _height + "px",
-			left: _left + "px",
-		};
-		setTimeout(function() {
-			cDiv.setAttribute("style", forStyle(finishStyle));
-			setTimeout(function() {
-				pDiv.removeChild(cDiv);
-			},
-			duration);
-		},
-		100)
-	}
-	document.querySelector('.wave').addEventListener('click',
-	function(e) {
-		show(e);
-	},
-	!1);
-},
-!1);
+	document.querySelector('#罩').addEventListener('click', e => show(e), !1);
+	document.querySelector('#生').addEventListener('click', e => show(e), !1);
+}, !1);
