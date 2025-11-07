710f7d8f289197186fa91d267acb008dae286ebb
TNPC <62324946+P-TNPC@users.noreply.github.com>
2025-11-07 19:00:15 +0800

    Nothing

--- PsRdInt/生成.js
+++ PsRdInt/生成.js
@@ -1,12 +1,8 @@
 function 出() {
-    var 量 = document.getElementById('量').value;
-    var 小 = document.getElementById('小').value; 小 = 小 ? 小 : 0;
-    var 大 = document.getElementById('大').value; 大 = 大 ? 大 : 0;
-    if (小 > 大) {
-        let 介 = 小;
-        小 = 大;
-        大 = 介;
-    }
+    var 量 = document.getElementById('量').value; 量 = ~~量;
+    var 小 = document.getElementById('小').value; 小 = ~~小;
+    var 大 = document.getElementById('大').value; 大 = ~~大;
+    if (小 > 大) [小, 大] = [大, 小];
     if (量 == '0') {
         var 出 = '呐，你知道吗？樱花下落的速度是秒速五厘米，是克里门特·沃洛什洛夫-2重型坦克的152.4毫米M-10T榴弹炮发射PB-35APCBC被帽风帽穿甲弹时炮口初速的八千七百二十分之一，是发射 OF-530HE榴弹时炮口初速的一万零六千分之一噢~<br>紧靠在一起的两颗心，会在500米的距离内，被穿甲弹炸出来的破片砸个稀巴烂，而榴弹里的6.86千克炸药会把他们轰得渣都不剩。<br>世界上没有什么力量比爱情更强大，除了KV-2重型坦克与它的152毫米主炮。';
         document.getElementById('出').className = '';
@@ -28,7 +24,7 @@ function 出() {
     document.getElementById('出').innerHTML = 出;
 }
 function 生(量, 小, 大) {
-    量 = 量 ? 量 : 1;
+    量 ||= 1;
     if (小 || 大) {
         var 组 = [];
         while (量--) {
